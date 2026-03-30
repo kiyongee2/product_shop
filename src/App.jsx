a707308d94cdaaec80e9e4e78a2c8bcfa4e3b239
kiyongee2 <tje0714@outlook.kr>
2026-03-30 20:43:16 +0900

    logout

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,32 @@ import ProductList from './pages/ProductList'
 import ProductInfo from './pages/ProductInfo'
 import AddProduct from './pages/AddProduct'
 import SignIn from './pages/SignIn'
+import { useState } from 'react'
 
 function App() {
+  //로그인 상태 관리
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  //로그인한 사용자 ID 관리
+  const [userId, setUserId] = useState('')
+
+  //로그인 핸들러
+  const handleLogin = (userId) => {
+    setIsLoggedIn(true); //로그인 성공시 상태 업데이트
+    setUserId(userId); //로그인한 사용자 ID 저장
+  }
+
+  //로그아웃 핸들러
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUserId('')
+  }
 
   return (
     <>
       <div>
         <BrowserRouter>
-          <Header />
+          <Header isLoggedIn={isLoggedIn} userId={userId} onLogout={handleLogout} />
           {/* <Main /> */}
           <Routes>
             <Route path='/' element={<Main />} />
@@ -22,7 +40,7 @@ function App() {
             {/* :id -> products/1과 같음  */}
             <Route path='/products/:id' element={<ProductInfo />} />
             <Route path='/add-product' element={<AddProduct />} />
-            <Route path='/signin' element={<SignIn />} />
+            <Route path='/signin' element={<SignIn onLogin={handleLogin} />} />
           </Routes>
         </BrowserRouter>
       </div>
